48117bd64d6f4f9643b398676627f549e36d5b13
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-15 22:15:35 +0900

    init

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,21 @@
-import EventEmitter from "events";
+import * as crypto from "crypto";
 
-export type eventTypedEmitter<E extends { [P in string]: unknown[] }> = {
-    emit<T extends keyof E>(event: T, ...args: E[T]): void;
+type OptionType = {
+    'slash'?: string;
+    'plus'?: string;
 };
 
-export type eventTypedAddListener<E extends { [P in string]: unknown[] }> = {
-    on<T extends keyof E>(event: T, listener: (...args: E[T]) => void): eventTypedAddListener<E>,
-    once<T extends keyof E>(event: T, listener: (...args: E[T]) => void): eventTypedAddListener<E>,
-    off<T extends keyof E>(event: T, listener: (...args: E[T]) => void): eventTypedAddListener<E>,
-};
-
-export type eventTypedEventEmitter<E extends { [P in string]: unknown[] }> = eventTypedEmitter<E> & eventTypedAddListener<E>;
-
-type TypedEventEmitterConstructor = { new <T extends { [P in string]: unknown[] }>(): eventTypedEventEmitter<T>; };
-
-export const TypedEventEmitter: TypedEventEmitterConstructor = EventEmitter as TypedEventEmitterConstructor;
-
-export function eventEmitterLogger<T extends eventTypedEmitter<{ [P in string]: unknown[] }>>(ee: T, logger: typeof console.log = console.log) {
-    const defaultEmit = ee.emit.bind(ee);
-
-    ee.emit = (type, ...arg) => {
-        logger(`event emitter emited. type: `, JSON.stringify(type), `, data: `, ...arg);
-        defaultEmit(type, ...arg);
-    };
-    return ee;
+function random(length: number = 32, option: OptionType) {
+    return crypto.randomBytes(Math.ceil(length * 3 / 4))
+        .toString('base64')
+        .slice(0, length)
+        .replaceAll('+', '$')
+        .replaceAll('/', '_')
+        .replaceAll('=', '');
 }
+
+export function generate(option: OptionType) {
+    return (length: number) => random(length, option);
+}
+
+export default random;
