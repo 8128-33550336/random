0c8d3935ebc0bd89235b9dd614d4873d4cc7a669
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-15 23:45:58 +0900

    add option

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import * as crypto from "crypto";
 
-type OptionType = {
-    'slash'?: string;
-    'plus'?: string;
-};
+type OptionType = [string, string];
 
-function random(length: number = 32, option: OptionType) {
+function random(length: number = 32, option?: OptionType) {
+    const [slash = '$', plus = '_'] = option || [];
     return crypto.randomBytes(Math.ceil(length * 3 / 4))
         .toString('base64')
         .slice(0, length)
-        .replaceAll('+', '$')
-        .replaceAll('/', '_')
-        .replaceAll('=', '');
+        .replaceAll('=', '')
+        .replaceAll('+', plus)
+        .replaceAll('/', slash);
 }
 
 export function generate(option: OptionType) {
