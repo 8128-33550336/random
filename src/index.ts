805c129b744f46234292ae34647bb0d230e27a61
8128-33550336 <66201155+8128-33550336@users.noreply.github.com>
2023-03-17 19:24:47 +0900

    random number

--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,8 @@ export function generate(option: OptionType) {
     return (length: number) => random(length, option);
 }
 
+export function number(digit: number) {
+    return (Math.floor(Math.random() * (10 ** digit)) + '').padStart(digit, '0');
+};
+
 export default random;
